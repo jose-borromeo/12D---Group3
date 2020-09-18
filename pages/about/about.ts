8419da4076fb9d19bbc6a2d9476d9e8c98fc0bd9
jose-borromeo <jose.borromeo080123@my.lsgh.edu.ph>
2020-09-18 17:56:50 +0800

    2 files modified, 1 added

--- pages/about/about.ts
+++ pages/about/about.ts
@@ -3,12 +3,27 @@ import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-about',
-  templateUrl: 'about.html'
+  templateUrl: 'about.html',
+  styleUrls: ['about.css'],
 })
 export class AboutPage {
+  BalanceTransferAmount:number;
+  PaymentTerms:number;
+  TotalInterestPayable: number;
+  TotalAmountPayable: number;
+  MonthlyPayment: number;
+
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  compute() {
+    this.TotalInterestPayable = ((this.BalanceTransferAmount*1.50/100)*this.PaymentTerms);
+
+    this.TotalAmountPayable = (this.BalanceTransferAmount+this.TotalInterestPayable);
+
+    this.MonthlyPayment = (this.TotalAmountPayable/this.PaymentTerms);
   }
 
 }
